912566351b2d52aafa6a4d22af84adf03c06f505
Isadora Abreu <isaabreucorrea@gmail.com>
2025-04-11 11:24:44 -0300

    Add files via upload

---
+++ exercicio4.py
@@ -0,0 +1,6 @@
+#LISTA DORIVAL - GIT HUB
+#EXERCICIO4
+numero = int(input("Digite um número para calcular seu fatorial: "))
+for fatorial in range(1,numero+1):
+    fatoracao = fatorial + numero
+    print(fatoracao)
