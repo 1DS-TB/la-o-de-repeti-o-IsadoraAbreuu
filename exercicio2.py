306421fbe0a45a6b20720208c011d3e87284471f
isaabreucorrea@gmail.com <seuemail@exemplo.com>
2025-04-14 19:11:11 -0300

    Adiciona o arquivo exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,9 @@
+#LISTA DORIVAL - GIT HUB
+#EXERCICIO 2
+contador = 0
+soma = 0
+numero = int(input("Digite um número: "))
+while numero <= contador:
+    contador += 1
+    soma = soma + contador
+print(contador)
