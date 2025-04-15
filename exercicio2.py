1a03e15731183529611ce3cd62efcd77f9249e52
Isadora Abreu <isaabreucorrea@gmail.com>
2025-04-14 22:09:16 -0300

    Segundo commit

---
+++ exercicio2.py
@@ -0,0 +1,9 @@
+#LISTA DORIVAL - GIT HUB
+#EXERCICIO 2
+n = int(input("Digite um número: "))
+contador = 1
+soma = 0
+while contador <= n:
+    soma += contador
+    contador += 1
+print(f"A soma de 1 até {n} é: {soma}")
--- exercicio4.py
+++ exercicio4.py
@@ -1,6 +1,7 @@
 #LISTA DORIVAL - GIT HUB
 #EXERCICIO4
+fatoracao = 1
 numero = int(input("Digite um número para calcular seu fatorial: "))
-for fatorial in range(1,numero+1):
-    fatoracao = fatorial + numero
-    print(fatoracao)
+for fatorial in range(1, numero +1):
+    fatoracao = fatoracao * fatorial
+print(f"O fatorial do número {numero} é {fatoracao}.")
