28671f5745ec23a8dc084444465f03271dfc7563
dora-bytes <isaabreucorrea@gmail.com>
2025-04-15 16:51:34 -0300

    Primeiro commit

---
+++ exercicio7.py
@@ -0,0 +1,7 @@
+#LISTA DORIVAL - GIT HUB
+#EXERCICIO 7
+usuario = int(input("Digite um número: "))
+for numero in range(1, usuario+1):
+    for asterisco in range(numero):
+        print("*", end="")
+    print()
