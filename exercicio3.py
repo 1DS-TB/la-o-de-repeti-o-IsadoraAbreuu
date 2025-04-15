2da15ee140b7d0806766211b33b210417a6feb6c
Isadora Abreu <isaabreucorrea@gmail.com>
2025-04-15 16:52:04 -0300

    Delete exercicio3.py

--- exercicio3.py
+++
@@ -1,6 +0,0 @@
-#LISTA DORIVAL - GIT HUB
-#EXERCÍCIO 3
-numero = int(input("Digite um número para saber sua tabuada: "))
-for multiplos in range(1,11):
-    tabuada = numero * multiplos
-    print(f"{numero} * {multiplos} = {tabuada}")
