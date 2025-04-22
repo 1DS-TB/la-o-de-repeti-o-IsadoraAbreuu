7448464250e6fccd31e53fd56ee66a22dbac240a
Isadora de Abreu <isaabreucorrea@gmail.com>
2025-04-22 17:18:04 -0300

    Primeiro commit

---
+++ exercicio9.py
@@ -0,0 +1,9 @@
+#LISTA DORIVAL - GIT HUB
+#EXERCICIO 9
+for numero in range(1, 10001):
+    soma_divisores = 0
+    for i in range(1, numero):
+        if numero % i == 0:
+            soma_divisores += i 
+    if soma_divisores == numero:
+        print(numero)
