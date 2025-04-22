585a71126c831deea25f6403c325182ca1d8e9d8
Isadora de Abreu <isaabreucorrea@gmail.com>
2025-04-22 16:49:08 -0300

    Segundo commit

---
+++ exercicio3.py
@@ -0,0 +1,6 @@
+#LISTA DORIVAL - GIT HUB
+#EXERCICIO 3
+numero = int(input("Digite um número para calcular sua tabuada: "))
+for i in range(1,11):
+    tabuada = numero * i
+    print(f"{numero} x {i} = {tabuada}")
